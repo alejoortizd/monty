e59ac94451b1258e7452dbe009a428ff53286d6a
daod2003 <1253@holbertonschool.com>
2020-01-06 02:09:49 +0000

    fixed add

--- _add.c
+++ _add.c
@@ -19,7 +19,7 @@ void _add(stack_t **stack, unsigned int count)
 	}
 	if (i < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", count);
+		fprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", count);
 		exit(EXIT_FAILURE);
 	}
 	node = (*stack)->next;
